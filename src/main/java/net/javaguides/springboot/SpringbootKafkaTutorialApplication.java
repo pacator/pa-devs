9f8c105d8684461c97bd1fa6c25ab9cfcf609563
Arnout Cator <arnout.cator@gmail.com>
2024-02-10 22:59:20 +0000

    initial commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: Arnout Cator
+ * Class SpringbootKafkaTutorialApplication
+ * Access point consumer
+ */
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+    }
+
+}
